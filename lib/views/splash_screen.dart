ce845fc1cf8700893b1dea4c33b94dd9b022a91e
shruti <shruti2000srivastava@gmail.com>
2021-07-05 00:00:39 +0530

    adding a splash screen #2

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFD1B9C6),
+      //backgroundColor: Color(0xFFD1B9C6),
       body: Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             SizedBox(
-              height: 200.0,
-              width: 200.0,
+              height: 400.0,
+              width: 400.0,
+              child: Lottie.asset('animation/pizza-loading.json'),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Pizza',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 56.0),
+                ),
+                Text(
+                  'Mania',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 56.0),
+                ),
+              ],
             )
           ],
         ),
